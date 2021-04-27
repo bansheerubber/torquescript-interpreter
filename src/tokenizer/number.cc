9db66c591d0380c47862248fdec5d3310742bb52
bansheerubber <gary.cactus.email@gmail.com>
2021-04-26 21:42:49 -0700

    stricter number tokenization

--- src/tokenizer/number.cc
+++ src/tokenizer/number.cc
@@ -18,24 +18,50 @@ Token Tokenizer::readNumber() {
 	};
 	
 	char character;
+	bool encounteredDot = false;
+	bool encounteredExponent = false;
 	while((character = this->getChar())) {
 		if(this->isNumber(character)) {
 			number.lexeme += character;
 		}
 		// handle exponents
 		else if(character == 'e' || character == 'E') {
+			if(encounteredExponent) {
+				this->error("did not expect multiple exponents in number");
+			}
+			encounteredExponent = true;
+			
 			number.lexeme += character;
 
 			character = this->getChar();
-			if(this->isNumber(character) || character == '-') {
+			if(this->isNumber(character)) {
+				number.lexeme += character;
+			}
+			else if(character == '-' || character == '+') {
+				number.lexeme += character;
+				character = this->getChar();
+
+				if(!this->isNumber(character)) {
+					this->error("expected number after exponent sign");
+				}
 				number.lexeme += character;
 			}
 			else {
-				this->error("invalid character '%c' after exponent point (number or negative sign must follow exponent symbol)", character);
+				this->error("invalid character '%c' after exponent point (number or sign must follow exponent symbol)", character);
 			}
 		}
 		// if this character is a decimal, we need the next character to be a number
 		else if(character == '.') {
+			if(encounteredDot) {
+				if(encounteredExponent) {
+					this->error("did not expect decimal point in exponent");
+				}
+				else {
+					this->error("did not expect multiple decimal points in number");
+				}
+			}
+			encounteredDot = true;
+
 			number.lexeme += character;
 
 			character = this->getChar();
