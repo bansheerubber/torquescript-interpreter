dd26ca34dcebe947fe5520c6b590ecbe17de8cfc
bansheerubber <gary.cactus.email@gmail.com>
2021-05-12 22:28:41 -0700

    implemented for loop

--- src/components/forBody.cc
+++ src/components/forBody.cc
@@ -58,5 +58,41 @@ string ForBody::print() {
 }
 
 ts::InstructionReturn ForBody::compile() {
-	return {};
+	ts::InstructionReturn output;
+
+	// final NOOP statement in for statement
+	ts::Instruction* noop = new ts::Instruction();
+	noop->type = ts::instruction::NOOP;
+
+	// add variable initialization
+	output.add(this->initialization->compile());
+
+	// add conditional
+	ts::InstructionReturn compiledConditional = this->conditional->compile();
+	output.add(compiledConditional);
+
+	// add conditional jump
+	ts::Instruction* conditionalJump = new ts::Instruction();
+	conditionalJump->type = ts::instruction::JUMP_IF_FALSE;
+	conditionalJump->jumpIfFalse.instruction = noop;
+	output.add(conditionalJump);
+
+	// add the body
+	for(Component* component: this->children) {
+		output.add(component->compile());
+	}
+
+	// add the increment
+	output.add(this->increment->compile());
+
+	// add jump to conditional
+	ts::Instruction* jump = new ts::Instruction();
+	jump->type = ts::instruction::JUMP;
+	jump->jump.instruction = compiledConditional.first;
+	output.add(jump);
+
+	// add final noop
+	output.add(noop);
+
+	return output;
 }
