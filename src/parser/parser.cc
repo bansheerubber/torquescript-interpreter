2964709f899f7ad2eee07489d827d4c07950e776
bansheerubber <gary.cactus.email@gmail.com>
2021-06-20 15:54:35 -0700

    remove last bits of code from when this was supposed to be a transpiler

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -4,38 +4,10 @@ Parser::Parser(Tokenizer* tokenizer, ParsedArguments args) {
 	this->newLine = "\n";
 	this->tab = "\t";
 	this->space = " ";
-	if(args.arguments["minify"] != "") {
-		this->newLine = "";
-		this->tab = "";
-		this->space = "";
-		this->minified = true;
-	}
-
-	string outputPath = tokenizer->fileName;
-	if(args.arguments["output"] != "") {
-		string directory = args.arguments["output"];
-		if(directory[directory.length() - 1] != '/') {
-			directory += '/';
-		}
-		outputPath = directory + outputPath;
-	}
 	
 	this->tokenizer = tokenizer;
 	this->sourceFile = new SourceFile(this);
 	Component::ParseBody(this->sourceFile, this->tokenizer, this);
-
-	// don't overwrite original file
-	if(args.arguments["piped"] != "") {
-		// printf("%s\n", this->sourceFile->print().c_str());
-	}
-	else {
-		if(outputPath != tokenizer->fileName) {
-			filesystem::create_directories(filesystem::path(outputPath).remove_filename());
-			ofstream file(outputPath);
-			file << this->sourceFile->print();
-			file.close();
-		}
-	}
 }
 
 Token Parser::expectToken(TokenType type1, TokenType type2, TokenType type3, TokenType type4, TokenType type5) {
