b593e88fcfcc470b13239ceac35819cbc89768d1
bansheerubber <gary.cactus.email@gmail.com>
2021-08-08 18:54:53 -0700

    fix potential problem with string copying

--- src/util/cloneString.cc
+++ src/util/cloneString.cc
@@ -6,7 +6,8 @@ using namespace std;
 char* cloneString(char* input) {
 	const size_t size = strlen(input);
 	char* output = new char[size + 1];
-	strncpy(output, input, size);
+	// strncpy(output, input, size);
+	memmove(output, input, size);
 	output[size] = '\0';
 	return output;
 }
