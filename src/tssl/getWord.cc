f3458403e1bcbfd5e19ff80e765f7bd34feb9da9
bansheerubber <gary.cactus.email@gmail.com>
2021-08-29 10:53:29 -0700

    refactor getWords to use generated code

--- src/tssl/getWord.cc
+++ src/tssl/getWord.cc
@@ -12,15 +12,7 @@ namespace ts {
 	namespace sl {
 		Entry* firstWord(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc >= 1) {
-				const char* words = args[0].stringData;
-				string first;
-				for(; *words; words++) {
-					char character = *words;
-					if(character == ' ' || character == '\t' || character == '\n') {
-						break;
-					}
-					first += character;
-				}
+				## tokenizing.py first " \t\n" args[0].stringData
 				return new Entry(stringToChars(first));
 			}
 
@@ -29,20 +21,7 @@ namespace ts {
 
 		Entry* restWords(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc >= 1) {
-				const char* words = args[0].stringData;
-				string rest;
-				bool copyDown = false;
-				for(; *words; words++) {
-					char character = *words;
-
-					if(copyDown) {
-						rest += character;
-					}
-
-					if(character == ' ' || character == '\t' || character == '\n') {
-						copyDown = true;
-					}
-				}
+				## tokenizing.py rest " \t\n" args[0].stringData
 				return new Entry(stringToChars(rest));
 			}
 
@@ -51,19 +30,7 @@ namespace ts {
 
 		Entry* getWord(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc >= 2) {
-				const char* words = args[0].stringData;
-				int count = args[1].numberData;
-				string word;
-				int spaceCount = 0;
-				for(; *words; words++) {
-					char character = *words;
-					if(character == ' ' || character == '\t' || character == '\n') {
-						spaceCount++;
-					}
-					else if(spaceCount == count) {
-						word += character;
-					}
-				}
+				## tokenizing.py getSingle " \t\n" args[0].stringData args[1].numberData
 				return new Entry(stringToChars(word));
 			}
 
@@ -72,32 +39,7 @@ namespace ts {
 
 		Entry* getWords(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc >= 2) {
-				const char* words = args[0].stringData;
-				int startCount = args[1].numberData;
-
-				if(startCount < 0) {
-					return nullptr;
-				}
-
-				int endCount = -1;
-				if(argc >= 3) {
-					endCount = args[2].numberData;
-				}
-				
-				string output;
-				int spaceCount = 0;
-				for(; *words; words++) {
-					char character = *words;
-					if(character == ' ' || character == '\t' || character == '\n') {
-						if(spaceCount >= startCount && (spaceCount < endCount || endCount == -1)) {
-							output += character;
-						}
-						spaceCount++;
-					}
-					else if(spaceCount >= startCount && (spaceCount <= endCount || endCount == -1)) {
-						output += character;
-					}
-				}
+				## tokenizing.py getMultiple " \t\n" args[0].stringData args[1].numberData args[2].numberData
 				return new Entry(stringToChars(output));
 			}
 
@@ -106,21 +48,7 @@ namespace ts {
 
 		Entry* getWordCount(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc == 1) {
-				const char* words = args[0].stringData;
-				double count = 0;
-
-				bool foundLast = false;
-				for(; *words; words++) {
-					char character = *words;
-					if(character == ' ' || character == '\t' || character == '\n') {
-						count++;
-						foundLast = false;
-					}
-					else {
-						foundLast = true;
-					}
-				}
-				count += foundLast;
+				## tokenizing.py getCount " \t\n" args[0].stringData
 				return new Entry((double)count);
 			}
 
@@ -129,32 +57,7 @@ namespace ts {
 
 		Entry* removeWord(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc >= 2) {
-				const char* words = args[0].stringData;
-				int count = args[1].numberData;
-				string output;
-				string currentWord;
-
-				int spaceCount = 0;
-				for(; *words; words++) {
-					char character = *words;
-					if(character == ' ' || character == '\t' || character == '\n') {
-						spaceCount++;
-						currentWord = "";
-					}
-					else {
-						currentWord += character;
-					}
-
-					if(spaceCount != count) {
-						if( // edge case for count = 0
-							!(character == ' ' || character == '\t' || character == '\n')
-							|| count != 0
-							|| spaceCount != 1
-						) {
-							output += character;
-						}
-					}
-				}
+				## tokenizing.py remove " \t\n" args[0].stringData args[1].numberData
 
 				// for some reason torquescript returns the last word if count > final space count
 				if(count > spaceCount) {
@@ -169,40 +72,7 @@ namespace ts {
 
 		Entry* setWord(Interpreter* interpreter, size_t argc, Entry* args) {
 			if(argc >= 3) {
-				const char* words = args[0].stringData;
-				int count = args[1].numberData;
-				const char* replace = args[2].stringData;
-				string output;
-				int spaceCount = 0;
-				bool replaced = false;
-				for(; *words; words++) {
-					char character = *words;
-					if(character == ' ' || character == '\t' || character == '\n') {
-						spaceCount++;
-
-						if(spaceCount == count) {
-							output += character;
-						}
-					}
-
-					if(spaceCount != count) {
-						output += character;
-					}
-					else if(replaced == false) {
-						replaced = true;
-						output += replace;
-					}
-				}
-
-				// edge case for when count >= getWordCount
-				for(int i = 0; i < count - spaceCount; i++) {
-					output += ' ';
-				}
-
-				if(count > spaceCount) {
-					output += replace;
-				}
-
+				## tokenizing.py set " \t\n" args[0].stringData args[1].numberData args[2].stringData
 				return new Entry(stringToChars(output));
 			}
 
